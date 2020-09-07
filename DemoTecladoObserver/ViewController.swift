4ee070db43cb699b714fb9db30b912a53931609c
Israel Parodi <israelps97@gmail.com>
2020-09-07 05:59:35 -0700

    Initial Commit

---
+++ DemoTecladoObserver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoTecladoObserver
+//
+//  Created by Israel Parodi on 9/7/20.
+//  Copyright © 2020 Israel Parodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
